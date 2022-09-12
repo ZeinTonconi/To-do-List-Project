81df2fe5957578dabd319f62bb9409cf920b10d4
Zein Tonconi <zeintonconi@gmail.com>
2022-09-12 12:46:56 -0400

    Creando las rutas

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,4 +9,30 @@ router.get('/',(req,res) => {
     })
 })
 
+router.post('/',(req,res) => {
+    res.json({
+        msg: 'Post - tasks'
+    })
+})
+
+router.put('/:id',(req,res) => {
+    const {id}=req.params;
+    res.json({
+        msg: `Put - Actualizar tarea ${id}`
+    })
+})
+
+router.put('/:id/complete',(req,res) =>{
+    const {id}=req.params;
+    res.json({
+        msg: `Put - Completar Tarea ${id}`
+    })
+})
+
+router.delete('/:id',(req,res) => {
+    const {id}=req.params;
+    res.json({
+        msg: `Delete - Elimiar tarea ${id}`
+    })
+})
 module.exports =router
