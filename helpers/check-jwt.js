c1177239d920def9f87c448c264bf76ea7dedaae
Zein Tonconi <zeintonconi@gmail.com>
2022-09-26 18:18:47 -0400

    feature - Verficador del JWT

---
+++ helpers/check-jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const checkJWT =  (req, res, next) => {
+    const token = req.header('keyToken');
+    if (!token) {
+        return res.status(401).json({
+            msg: "No hay token en la peticion"
+        })
+    }
+
+    try{
+        jwt.verify(token,process.env.SECRET_OR_PRIVATEKEY);
+        next();
+    }
+    catch(err) {
+        //console.log(err);
+        res.status(401).json({
+            msg: "Token no valido"
+        })
+    }
+}
+
+module.exports = {
+    checkJWT
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { tasksGet, tasksPost, tasksDelete, putTask, putCompleteTask } = require('../controllers/tasks');
+const { checkJWT } = require('../helpers/check-jwt');
 const { isTaskInDB } = require('../helpers/dbValidator');
 const { validateCamp } = require('../middlewares/validateCamps');
 
@@ -10,27 +11,33 @@ const { validateCamp } = require('../middlewares/validateCamps');
 const router = Router();
   
 
-router.get('/', tasksGet);
+router.get('/', [
+    checkJWT
+],tasksGet);
 
 router.post('/', [
+    checkJWT,
     check('descr','La Tarea necesita una descripcion').notEmpty(),
     check('category', 'La Tarea necesita una categoria').notEmpty(),
     validateCamp
 ], tasksPost )
 
 router.put('/:id', [
+    checkJWT,
     check('id',"El id no es valido").isNumeric(),
     validateCamp,
     isTaskInDB
 ], putTask)
 
 router.put('/:id/complete', [
+    checkJWT,
     check('id', 'El id no es valido').isNumeric(),
     validateCamp,
     isTaskInDB
 ], putCompleteTask)
 
 router.delete('/:id',[
+    checkJWT,
     check('id').isNumeric(),
     validateCamp,
     isTaskInDB
